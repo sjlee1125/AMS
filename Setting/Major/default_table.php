a60bb10271e1826a0f1e78462db48a54e7d027eb
Seungjun Lee <seung9779@gmail.com>
2019-08-04 18:15:03 +0900

    Update default_table.php

--- Setting/Major/default_table.php
+++ Setting/Major/default_table.php
@@ -2,7 +2,7 @@
 	include_once "../../dbconnect.php";
 	require '../../JSON.php'; 
 	
-  	$sql = "select major.idx,major.MajorName,major.MajorCode,count(major.idx) as count from major left join student on major.idx=student.Major group by major.idx";
+  	$sql = "select major.idx,major.MajorName,major.MajorCode,count(major.idx) as count from major left outer join student on major.idx=student.Major group by major.idx";
   	$result=$conn->query($sql);
 
   	$rows=array();
